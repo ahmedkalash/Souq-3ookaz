7bd5467f694a23ef1e36dbed1c59d9019f1a93d2
Ahmed Kalash <ahmed.kalash@scandiweb.com>
2024-02-14 04:01:32 +0200

    Fix installation issue

--- database/migrations/2023_11_27_113250_create_product_attributes_table.php
+++ database/migrations/2023_11_27_113250_create_product_attributes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('product_attributes', function (Blueprint $table) {
             $table->id();
             $table->foreignId('product_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
-            $table->text('attribute_name')->unique();
+            $table->string('attribute_name')->unique();
             $table->text('value');
             $table->timestamps();
         });
